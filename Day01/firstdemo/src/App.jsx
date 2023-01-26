e318a97e07ce0a4d088db881d4a97064e14dca20
sara53 <sara.atef001@gmail.com>
2023-01-26 10:28:11 +0200

    InitalDemo

---
+++ Day01/firstdemo/src/App.jsx
@@ -0,0 +1,21 @@
+import "./App.css";
+import { Content } from "./componets/content";
+import Footer from "./componets/footer";
+
+import { Header } from "./componets/header";
+export function App() {
+  return (
+    <div className="">
+      <Header></Header>
+      <Content />
+      <Footer />
+    </div>
+  );
+}
+
+/**
+ * Class Component
+ *========================= 
+    JSX -- javascript xml --- javascript Code
+ *
+ */
